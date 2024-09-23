64a7943c9f9e0af728f300ad6a0528ec8135791c
Duduzile95 <ndhlovududuzile95@gmail.com>
2024-09-23 11:19:57 +0200

    added basic Javascript and intergated weather API to my html.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,56 @@
+
+function formattedDate(){
+let currentDate = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[currentDate.getDay()];
+let hours = currentDate.getHours().toString().padStart(2, '0'); 
+    let minutes = currentDate.getMinutes().toString().padStart(2, '0'); 
+    let time = `${hours}:${minutes}`; 
+
+  let dateString = `${day} ${time}`;
+
+  let updateDate = document.getElementById("current-date");
+  if (updateDate) {
+        updateDate.innerHTML = dateString;
+    } else {
+        console.error("not found");
+    }
+};
+
+formattedDate();
+
+
 function searchCity(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-let cityInput = document.getElementById("city-button");
-if (cityInput) {
-    let city = cityInput.value.trim();
+  
+  let cityInput = document.getElementById("city-button");
+
+  if (cityInput) {
+    let city = cityInput.value.trim();  
+
     if (city) {
-        let cityNameElement = document.getElementById("city-name");
-        if (cityNameElement) {
-            cityNameElement.innerHTML = city;
- 
-            let apiKey = "d270ff52dt3f3obd13aba943d507af34";
+      let cityNameElement = document.getElementById("city-name");
+
+      if (cityNameElement) {
         
-            let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+        cityNameElement.innerHTML = city;
 
         
+        let apiKey = "d270ff52dt3f3obd13aba943d507af34";
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
         
-            axios.get(apiUrl).then(showTemperature);
+        axios.get(apiUrl).then(showTemperature);
           
           };
       } else {
@@ -23,6 +58,21 @@ if (cityInput) {
       }
     } 
   
-            
-        }
+
+
+  function showTemperature(response) {
+    if (response.data && response.data.temperature) {
+    let temperature = Math.round(response.data.temperature.current);
+
+    let temperatureElement = document.querySelector("#temp");
+    if (temperatureElement){
+    temperatureElement.innerHTML = `${temperature}°C`;
+    } else{
+      console.error("notfound.");
+    }
     
+    }
+  }
+}
+document.getElementById("city-form");
+document.addEventListener("submit", searchCity);
