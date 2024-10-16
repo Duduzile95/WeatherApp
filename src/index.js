d27e4434ba79a257fcb5c6962ddc97cee51e29e0
Duduzile95 <ndhlovududuzile95@gmail.com>
2024-10-16 14:03:01 +0200

    modified the code

--- src/index.js
+++ src/index.js
@@ -85,27 +85,28 @@ document.getElementById("city-form");
 document.addEventListener("submit", searchCity);
 showTemperature();
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
 
-function displayForecast(){
-  let forecastElement = document.querySelector("#forecast");
-let days = [ "Tue", "Wed", "Thu", "Fri", "Sat"];
-let forecastHtml = "";
-
-days.forEach(function (day) {
-forecastHtml= forecastHtml +
-      `
-        <div class="weather-app-forecast" >
+  days.forEach(function (day) {
+    forecastHtml += `
+      <div class="weather-app-forecast">
         <div class="weather-forecast-day"></div>
-        <div class="weather-forecast-date"> ${day}</div>
-          <div class="weather-forecast-icon">☀️</div> 
-          <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-temperature"><strong>15°C</strong> </div>
-
-            <div class="weather-forecast-temperature"> 8°C</div> 
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">☀️</div> 
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature"><strong>15°C</strong></div>
+          <div class="weather-forecast-temperature">8°C</div> 
         </div>
-         `;
-} );
+      </div>
+    `;
+  });
+
+  
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
-forecastElement.innerHTML = forecastHtml;
+
 displayForecast();
